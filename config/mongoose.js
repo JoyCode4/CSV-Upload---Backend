c8fe38a5a74dde41cf71f308a0b2e1e3ed4781b3
JoyCode4 <joywadhonkar4@gmail.com>
2024-02-11 12:52:44 +0530

    1.updated the code using mongodb atlas link 2.update the git igonre 3.created the .env file and use the dotenv package to access that

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
+const url = process.env.MONGODB_URL;
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/csv");
+  await mongoose.connect(url);
   console.log("DB is connected");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
